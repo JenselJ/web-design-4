45827d2d2e860a005d9f052bdc0d5c37d2dcc2cb
JenselJ <saneer2002@gmail.com>
2023-06-07 22:07:27 +1000

    writing code for line seperation in js

--- script.js
+++ script.js
@@ -18,15 +18,49 @@ window.addEventListener("load", function () {
       this.canvasHeight = canvasHeight;
       this.textX = this.canvasWidth / 2;
       this.textY = this.canvasHeight / 2;
+      this.fontSize = 100;
+      this.lineHeight = this.fontSize * 0.8;
+      this.maxTextWidth = this.canvasWidth * 0.8;
     }
     wrapText(text) {
+      const gradient = this.context.createLinearGradient(
+        0,
+        0,
+        this.canvasWidth,
+        this.canvasHeight
+      );
+      gradient.addColorStop(0.3, "red");
+      gradient.addColorStop(0.5, "fuchsia");
+      gradient.addColorStop(0.7, "purple");
+      this.context.fillStyle = gradient;
+      this.context.textAlign = "center";
+      this.context.textBaseline = "middle";
+      this.context.lineWidth = 3;
+      this.context.strokeStyle = "white";
+      this.context.font = this.fontSize + "px Helvetica";
       this.context.fillText(text, this.textX, this.textY);
+      this.context.strokeText(text, this.textX, this.textY);
+      // break multiline text
+      let linesArray = [];
+      let words = text.split("");
+      let lineCounter = 0;
+      let line = "";
+      for (let i = 0; i < words.length; i++) {
+        let testLine = line + words[i] + " ";
+        if (this.context.measureText(testLine).width > this.maxTextWidth) {
+          line = words[i] + " ";
+          lineCounter++;
+        } else {
+          line = testLine;
+        }
+      }
     }
     convertToParticles() {}
     render() {}
   }
 
   const effect = new Effect(ctx, canvas.width, canvas.height);
+  effect.wrapText("Hello");
   console.log(effect);
 
   function animate() {}
@@ -61,26 +95,26 @@ window.addEventListener("load", function () {
   // const lineHeight = 80;
 
   // function wrapText(text) {
-  //   let linesArray = [];
-  //   let lineCounter = 0;
-  //   let line = "";
-  //   let words = text.split(" ");
-  //   for (let i = 0; i < words.length; i++) {
-  //     let testLine = line + words[i] + " ";
-  //     if (ctx.measureText(testLine).width > maxTextWidth) {
-  //       line = words[i] + " ";
-  //       lineCounter++;
-  //     } else {
-  //       line = testLine;
-  //     }
-  //     linesArray[lineCounter] = line;
+  // let linesArray = [];
+  // let lineCounter = 0;
+  // let line = "";
+  // let words = text.split(" ");
+  // for (let i = 0; i < words.length; i++) {
+  //   let testLine = line + words[i] + " ";
+  //   if (ctx.measureText(testLine).width > maxTextWidth) {
+  //     line = words[i] + " ";
+  //     lineCounter++;
+  //   } else {
+  //     line = testLine;
   //   }
-  //   let textHeight = lineHeight * lineCounter;
-  //   let textY = canvas.height / 2 - textHeight / 2;
-  //   linesArray.forEach((el, index) => {
-  //     ctx.fillText(el, canvas.width / 2, textY + index * lineHeight);
-  //   });
-  //   console.log(linesArray);
+  //   linesArray[lineCounter] = line;
+  // }
+  // let textHeight = lineHeight * lineCounter;
+  // let textY = canvas.height / 2 - textHeight / 2;
+  // linesArray.forEach((el, index) => {
+  //   ctx.fillText(el, canvas.width / 2, textY + index * lineHeight);
+  // });
+  // console.log(linesArray);
   // }
 
   // textInput.addEventListener("keyup", function (e) {
